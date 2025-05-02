83269c71750a38005e18086278a4843e3d499d60
Sean Heckathorne <heckathorne.s@gmail.com>
2025-05-01 20:06:04 -0500

    finished the redesign of the ride card

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,7 +97,8 @@ LOCAL_APPS = [
     "group_velo.data.apps.DataConfig",
     "group_velo.routes.apps.RoutesConfig",
     "group_velo.events.apps.EventsConfig",
-    "group_velo.WeatherConfig" "group_velo.notifications.apps.NotificationsConfig",
+    "group_velo.weather.apps.WeatherConfig",
+    "group_velo.notifications.apps.NotificationsConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
